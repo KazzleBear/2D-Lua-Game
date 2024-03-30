511348cb008e1e3e7f787fd556a90f19e2c40c7d
KazzleBear <media3d2@gmail.com>
2024-03-30 14:08:33 -0700

    moved stuff to main

--- main.lua
+++ main.lua
@@ -3,33 +3,66 @@ require("render")
 --You can use Alt + L to open a love2d window
 _G.love = require("love")
 
+local SCREEN_WIDTH, SCREEN_HEIGHT = love.graphics.getDimensions()
+local HALF_WIDTH = SCREEN_WIDTH / 2
+local HALF_HEIGHT = SCREEN_HEIGHT / 2
 
 
 
-cubeCopy = ("render").cube
-
-cubeCopy[1] = ("render").normalize(cubeCopy[1]) 
-
-
-
-
-
-
-a = 0.5
+local FOV = 30
+local NEAR = 0.1
+local FAR = 1000
+local a = SCREEN_HEIGHT/SCREEN_WIDTH
+local fovRad = 1/math.tan((0.5*FOV) * math.pi/180) --arctan of theta/2
 
 
 
+local vector3d = {
+    x, y, z
+}
+
+local triangle = {
+    vector3d.x, vector3d.y, vector3d.z
+}
+
+local mesh = {
+    triangles = {}
+}
+
+
+cube = mesh
+
+
+cube.triangles = {
+    --top
+    {0, 1, 0,       0, 1, 1,       1, 1, 1},
+    {0, 1, 0,       1, 1, 1,       1, 1, 0},
+    --bottom
+    {0, 0, 0,       0, 0, 1,       1, 0, 1},
+    {0, 0, 0,       1, 0, 1,       1, 0, 0},
+    --right
+    {1, 0, 0,       1, 1, 0,       1, 1, 1},
+    {1, 0, 0,       1, 1, 1,       1, 0, 1},
+    --left
+    {0, 0, 0,       0, 1, 0,       0, 1, 1},
+    {0, 0, 0,       0, 1, 1,       0, 0, 1},
+    --close
+    {0, 0, 0,       0, 1, 0,       1, 1, 0},
+    {0, 0, 0,       1, 1, 0,       1, 0, 0},
+    --far
+    {0, 0, 1,       0, 1, 1,       1, 1, 1},
+    {0, 0, 1,       1, 1, 1,       1, 0, 1}
+}
 
 
 
-function love.load()
-    
-    love.graphics.setBackgroundColor(a, 0.5, 1)
-end
 
-function love.update(dt)
-a = a + 1.1
-end
+
+
+
+
+
+
 
 function love.draw()
     --Uses RGB (normally it's a number between 0 and 255, but for lua its 0-1
--- render.lua
+++ render.lua
@@ -2,17 +2,7 @@ local render = {}
 
 
 --// Main Settings
-local SCREEN_WIDTH, SCREEN_HEIGHT = love.graphics.getDimensions()
-local HALF_WIDTH = SCREEN_WIDTH / 2
-local HALF_HEIGHT = SCREEN_HEIGHT / 2
 
-
-
-local FOV = 30
-local NEAR = 0.1
-local FAR = 1000
-local a = SCREEN_HEIGHT/SCREEN_WIDTH
-local fovRad = 1/math.tan((0.5*FOV) * math.pi/180) --arctan of theta/2
 
 
 local WhiteColor = {1,1,1}
@@ -39,42 +29,7 @@ render.Errors = {
 }
 
 
-local vector3d = {
-    x, y, z
-}
 
-local triangle = {
-    vector3d.x, vector3d.y, vector3d.z
-}
-
-local mesh = {
-    triangles = {}
-}
-
-
-
-cube = mesh
-
-cube.triangles = {
-    --top
-    {0, 1, 0,       0, 1, 1,       1, 1, 1},
-    {0, 1, 0,       1, 1, 1,       1, 1, 0},
-    --bottom
-    {0, 0, 0,       0, 0, 1,       1, 0, 1},
-    {0, 0, 0,       1, 0, 1,       1, 0, 0},
-    --right
-    {1, 0, 0,       1, 1, 0,       1, 1, 1},
-    {1, 0, 0,       1, 1, 1,       1, 0, 1},
-    --left
-    {0, 0, 0,       0, 1, 0,       0, 1, 1},
-    {0, 0, 0,       0, 1, 1,       0, 0, 1},
-    --close
-    {0, 0, 0,       0, 1, 0,       1, 1, 0},
-    {0, 0, 0,       1, 1, 0,       1, 0, 0},
-    --far
-    {0, 0, 1,       0, 1, 1,       1, 1, 1},
-    {0, 0, 1,       1, 1, 1,       1, 0, 1}
-}
 
 normalize = {
     function(table)
